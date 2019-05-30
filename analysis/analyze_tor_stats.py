5b3184ad92447d6ff4414c5b601fb59bab7599c2
ohhisara <sarabmoreira1372@gmail.com>
2019-05-30 14:28:48 +0100

    tudo atualizado

--- analysis/analyze_tor_stats.py
+++ analysis/analyze_tor_stats.py
@@ -6,8 +6,9 @@
 #       {0...1646}
 import pandas as pd 
 import matplotlib.pyplot as plt
+import numpy as np
 
-main_dict = eval(open("/home/sara/Desktop/Meh/SR/shadow/shadow-plugin-tor/tools/stats.tor.json").read())
+main_dict = eval(open("/home/sara/Desktop/Meh/SR/shadow/shadow-plugin-tor/resource/tor-sr/final-results/stats.tor.json").read())
 
 d = {}
 names = []
@@ -15,9 +16,30 @@ i=0
 for x,item in main_dict.items():
     for y, values in item.items():
         names.insert(i,str(y))
+        i=i+1
         d[y] = pd.DataFrame(values)
-
 result = pd.concat(d,keys=names)
-print result
 result.to_csv('out_tor_stats.csv')
+
+#conn = []
+#j=0
+#for y in d:
+#    for x in d['torclient1'].bytes_written.values:
+#        for z in d[y].bytes_read.values:
+#            if x == z & not (y in conn):
+#                conn.insert(j,y)
+#                j=j+1
+#print conn
+
+#plt.plot(d['torclient1'].bytes_written)
+#plt.show()
+#plt.plot(d['middle1'].bytes_read)
+#plt.show()
+
+
+#print result
+
+
+
+
     
